a9a62b1d1ca469c1a6be2a4c5f2fcb04ca254470
tiancechen <chen0650@algonquinlive.com>
2018-10-31 14:59:04 -0400

    lab 5

---
+++ app/src/main/java/com/example/tianc/androidlabs/ChatWindow.java
@@ -0,0 +1,133 @@
+package com.example.tianc.androidlabs;
+
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.tianc.androidlabs.sampledata.ChatDatabaseHelper;
+
+import java.util.ArrayList;
+
+import static com.example.tianc.androidlabs.StartActivity.ACTIVITY_NAME;
+
+
+public class ChatWindow extends Activity {
+
+    ListView chatView;
+    Button buttonChat;
+    EditText editText;
+    ArrayList<String> messages;
+    ChatDatabaseHelper dbManager;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_window);
+
+        /* lab4 */
+        chatView = findViewById(R.id.chatView);
+        buttonChat = findViewById(R.id.button4);
+        editText = findViewById(R.id.editText3);
+        messages = new ArrayList<>();
+
+        final ChatAdapter messageAdapter;//this means chatWindow
+        messageAdapter = new ChatAdapter(this);
+        chatView.setAdapter(messageAdapter);
+
+        dbManager = new ChatDatabaseHelper(this);
+        final SQLiteDatabase db = dbManager.getWritableDatabase();
+
+        String selectQurey = "select * FROM " + dbManager.TABLE_NAME;
+
+        Cursor cursor = db.rawQuery(selectQurey,null);
+        while (cursor.moveToNext())
+
+        {
+            String newMessage = cursor.getString(cursor.getColumnIndex(dbManager.KEY_MESSAGE));
+            messages.add(newMessage);
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + newMessage);
+        }
+
+        Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + cursor.getColumnCount() );
+
+
+        for (int columnIndex = 0; columnIndex < cursor.getColumnCount(); columnIndex++){
+            Log.i(ACTIVITY_NAME, "Column Name:" + cursor.getColumnName(columnIndex));
+        }
+
+        buttonChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = editText.getText().toString();
+                messages.add(data);
+                messageAdapter.notifyDataSetChanged();
+                //Insert the new message into the database, contentValues object will put the new message
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(dbManager.KEY_MESSAGE,editText.getText().toString());
+
+                long insertCheck = db.insert(dbManager.TABLE_NAME,null,contentValues);
+                Log.i("StartChat", "insert data result: " + insertCheck);
+                editText.setText("");
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        dbManager.close();
+    }
+
+
+
+    private class ChatAdapter extends ArrayAdapter<String> {
+        public ChatAdapter(Context ctx) {
+            super(ctx, 0);
+        }
+
+        @Override
+        public int getCount() {
+            return messages.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return messages.get(position);
+        }
+
+        @Override
+        public long getItemId(int position){
+            return super.getItemId(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result = null;
+            if(position % 2 == 0) result = inflater.inflate(R.layout.chat_rot_incoming,null);
+            else result = inflater.inflate(R.layout.chat_row_outgoing,null);
+
+            TextView message = (TextView) result.findViewById(R.id.message_text);
+            message.setText(getItem(position));
+            return result;
+        }
+
+    }
+}
--- app/src/main/java/com/example/tianc/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/tianc/androidlabs/StartActivity.java
@@ -1,136 +1,103 @@
 package com.example.tianc.androidlabs;
 
 
-
-
 import android.app.Activity;
-
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.util.Log;
-
 import android.view.View;
-
 import android.widget.Button;
-
 import android.widget.Toast;
 
 
 
 public class StartActivity extends Activity {
-
-
-
+    Button btn, btn2;
     protected static final String ACTIVITY_NAME = "StartActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+
+
+                int REQUEST_CODE = 50;
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+        /* this is lab 4
+         */
+        btn2 = findViewById(R.id.button3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(ACTIVITY_NAME, "User clicked Start Chat");
+                Intent intent = new Intent(StartActivity.this, ChatWindow.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }// end in on create
 
 
 
     @Override
-
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_start);
-
-        Log.i(ACTIVITY_NAME,"In onCreate()");
-
-
-
-        final Button button = (Button)findViewById(R.id.button);
-
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v){
-
-                Intent intent = new Intent(StartActivity.this,ListItemsActivity.class);
-
-                startActivityForResult(intent, 10);
-
-            }
-
-        });
-
-    }
-
-
-
-    protected void onActivityResult(int requestCode, int responseCode, Intent data){
-
-        if (requestCode == 10){
-
-            Log.i(ACTIVITY_NAME,"Returned to StartActivity.onActivityResult");
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == 50) {
+            String messagePassed = data.getStringExtra("Response")+" ListItemsActivity passed: My information to share";
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+            Toast.makeText(this,messagePassed,
+                    Toast.LENGTH_LONG).show();
         }
-
-
-
-        if (responseCode == Activity.RESULT_OK) {
-
-            String messagePassed = data.getStringExtra("Response");
-
-            CharSequence text = "ListItemActivity passed: ";
-
-            Toast.makeText(getApplicationContext(), text+messagePassed, Toast.LENGTH_LONG).show(); //this is the ListActivity
-
-        }
-
     }
 
 
 
-    protected void onResume(){
-
-        super.onResume();
-
-        Log.i(ACTIVITY_NAME, "In onResume()");
-
-    }
-
-
-
-    protected void onStart(){
-
+    @Override
+    public void onStart(){
         super.onStart();
+        Log.i("ACTIVITY_NAME","IN onStart()");
+    }
 
-        Log.i(ACTIVITY_NAME,"In onStart()");
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i("ACTIVITY_NAME","IN onResume()");
 
     }
 
 
-
-    protected void onPause(){
-
+    @Override
+    public void onPause(){
         super.onPause();
-
-        Log.i(ACTIVITY_NAME,"In onPause()");
+        Log.i("ACTIVITY_NAME","IN onPause()");
 
     }
 
 
-
-    protected void onStop(){
-
+    @Override
+    public void onStop(){
         super.onStop();
-
-        Log.i(ACTIVITY_NAME,"In onStop()");
+        Log.i("ACTIVITY_NAME","IN onStop()");
 
     }
 
-
-
-    protected void onDestroy(){
-
+    @Override
+    public void onDestroy(){
         super.onDestroy();
-
-        Log.i(ACTIVITY_NAME,"In onDestroy()");
+        Log.i("ACTIVITY_NAME","IN  onDestroy()");
 
     }
-
-
 
 }
